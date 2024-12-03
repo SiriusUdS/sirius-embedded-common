c315ae52f05ec8a7eea418b78114fd8a4e02cee5
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2024-12-03 01:58:25 -0500

    Implementation of default accelerometer functions

--- Inc/Accelerometer/Accelerometer.h
+++ Inc/Accelerometer/Accelerometer.h
@@ -1,17 +1,21 @@
 #pragma once
 
+#include "../../sirius-headers-common/Accelerometer/AccelerometerData.h"
+
 struct Accelerometer;
 
 typedef void (*Accelerometer_init)(struct Accelerometer* instance);
 
-typedef void (*Accelerometer_gatherData)(struct Accelerometer* instance);
+typedef void (*Accelerometer_gatherData)(struct Accelerometer* instance, AccelerometerData* rawData);
 
 typedef struct {
   Accelerometer_init initFunction;
   Accelerometer_gatherData gatherDataFunction;
+
+  AccelerometerStatus status;
 }
 Accelerometer;
 
 extern void Accelerometer_initDefault(Accelerometer* instance);
 
-extern void Accelerometer_gatherDataDefault(Accelerometer* instance);
+extern void Accelerometer_gatherDataDefault(Accelerometer* instance, AccelerometerData* rawData);
--- Src/Accelerometer/Accelerometer.c
+++ Src/Accelerometer/Accelerometer.c
@@ -1,9 +1,15 @@
 #include "../../Inc/Accelerometer/Accelerometer.h"
 
 void Accelerometer_initDefault(Accelerometer* instance) {
-
+  instance->status.value = 0;
+  instance->status.bits.invalidFunctionPointer = 1;
 }
 
-void Accelerometer_gatherDataDefault(Accelerometer* instance) {
-  
+void Accelerometer_gatherDataDefault(Accelerometer* instance, AccelerometerAxis* rawData) {
+  instance->status.value = 0;
+  instance->status.bits.invalidFunctionPointer = 1;
+
+  rawData->x = 0;
+  rawData->y = 0;
+  rawData->z = 0;
 }

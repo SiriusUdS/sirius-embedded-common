02ed1bbca588400dca0d446a0cc444879358db7f
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2024-12-04 02:01:24 -0500

    Move Common sensor headers from engine project

---
+++ Inc/Sensors/PressureSensor/PressureSensor.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdint.h>
+
+struct PressureSensor;
+
+typedef void (*PressureSensor_init)(struct PressureSensor* instance);
+
+typedef void (*PressureSensor_gatherData)(struct PressureSensor* instance);
+
+typedef struct {
+  PressureSensor_init initFunction;
+}
+PressureSensor;
+
+extern void PressureSensor_initDefault(PressureSensor* instance);
---
+++ Src/PressureSensor/PressureSensor.c
@@ -0,0 +1,5 @@
+#include "../../Inc/PressureSensor/PressureSensor.h"
+
+void PressureSensor_initDefault(PressureSensor* instance) {
+  return 1;
+}

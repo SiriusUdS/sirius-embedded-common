8275788d1973e8e7b26bed25149c28defb16227d
marcG05 <marcantoine.lavigne.gauthier@gmail.com>
2025-01-06 15:02:36 -0500

    Valve 1.0

--- Inc/Devices/Valve/Valve.h
+++ Inc/Devices/Valve/Valve.h
@@ -1,10 +1,24 @@
 #pragma once
 
+// VERIFY THIS SECTION
+#include "stm32f4xx_hal.h"
+#include "stm32f411xe.h"
+#include <stdint.h>
+
+#define PWM_DUTY_CYCLE_MIN 0
+#define PWM_DUTY_CYCLE_MAX 100 // FOR ARR OF 100
+//SET THE PWM CONTROLLER TO CHANNEL 1 AND THE ARR FOR THE DESIRE FREQ
+//SOURCE : https://controllerstech.com/pwm-in-stm32/
+
+#define ELAPSED_DELAY_MS 50
+#define ELAPSED_STEP 1
+
 struct Valve;
 
 typedef enum {
   VALVE_CLOSED,
-  VALVE_OPENED
+  VALVE_OPENED,
+  VALVE_PENDING
 }
 ValveState;
 
@@ -16,7 +30,16 @@ typedef void (*Valve_setState)(struct Valve* instance, ValveState state);
 
 typedef struct {
   Valve_init initFunction;
+  ValveState state;
+  uint16_t actualCycle;
+  unsigned long CycleLastTime;
 }
 Valve;
+
+
+void VALVE_init();
+void VALVE_SetState(ValveState _state);
+uint16_t VALVE_GetActualCycle();
+ValveState VALVE_GetState();
 
 extern void Valve_initDefault(Valve* instance);
---
+++ Src/Valve/Valve.c
@@ -0,0 +1,87 @@
+#include "Valve.h"
+
+static volatile Valve valve;
+
+static int closeState();
+
+static int openState();
+
+static void setCCR(uint16_t cycle);
+
+
+TIM_HandleTypeDef htim1;
+
+void VALVE_init(){
+    setCCR(PWM_DUTY_CYCLE_MIN);
+    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+    valve.state = VALVE_CLOSED;
+}
+
+void VALVE_SetState(ValveState _state){
+    if(_state == valve.state) return;
+    int operation = 0;
+
+    switch(_state){
+        case VALVE_CLOSED:
+            operation = closeState();
+            break;
+        case VALVE_OPENED:
+            operation = openState();
+            break;
+        default:
+            break;
+    }
+
+    if(operation == 0){
+        valve.state = VALVE_PENDING;
+        return;
+    }
+
+    valve.state = _state;
+}
+
+uint16_t VALVE_GetActualCycle(){
+    return valve.actualCycle;
+}
+
+ValveState VALVE_GetState(){
+    return valve.state;
+}
+
+void setCCR(uint16_t cycle){
+    if(cycle < PWM_DUTY_CYCLE_MIN){
+        cycle = PWM_DUTY_CYCLE_MIN;
+    }
+
+    if(cycle > PWM_DUTY_CYCLE_MAX){
+        cycle = PWM_DUTY_CYCLE_MAX;
+    }
+
+    TIM1->CCR1 = cycle;
+    valve.actualCycle = cycle;
+    valve.CycleLastTime = HAL_GetTick();
+}
+
+
+int closeState(){
+    if(valve.actualCycle == PWM_DUTY_CYCLE_MIN) return 1;
+
+    if(HAL_GetTick() - valve.CycleLastTime >= ELAPSED_DELAY_MS){
+        setCCR(valve.actualCycle - ELAPSED_STEP);
+    }
+
+    return 0;
+}
+
+int openState(){
+    if(valve.actualCycle == PWM_DUTY_CYCLE_MAX) return 1;
+
+    if(HAL_GetTick() - valve.CycleLastTime >= ELAPSED_DELAY_MS){
+        setCCR(valve.actualCycle + ELAPSED_STEP);
+    }
+
+    return 0;
+}
+
+
+

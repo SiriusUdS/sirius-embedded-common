4084753c0dde03bdba0a6be305eab5745fa373d8
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-02-01 01:43:31 -0500

    Correction in error status register usage (separation)

--- Src/Sensor/PressureSensor/PressureSensor.c
+++ Src/Sensor/PressureSensor/PressureSensor.c
@@ -2,13 +2,12 @@
 
 void PressureSensor_initDefault(PressureSensor* instance) {
   instance->status.value = 0;
-  instance->status.bits.invalidFunctionPointer = 1;
+  instance->errorStatus.value = 0;
+  instance->errorStatus.bits.invalidFunctionPointer = 1;
 
   instance->gatherData = PressureSensor_gatherDataDefault;
 }
 
 void PressureSensor_gatherDataDefault(PressureSensor* instance) {
-  instance->status.bits.invalidFunctionPointer = 1;
-
-  
+  instance->errorStatus.bits.invalidFunctionPointer = 1;
 }

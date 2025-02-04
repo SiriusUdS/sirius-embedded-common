62054b385fcf0ede15ca0f8aa594e51195f3dd4f
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-02-04 02:10:08 -0500

    Use casts to remove warnings, create UART and GPIO files

--- Src/Device/Valve/Valve.c
+++ Src/Device/Valve/Valve.c
@@ -5,15 +5,16 @@ void Valve_initDefault(Valve* instance) {
   instance->errorStatus.value = 0;
   instance->errorStatus.bits.defaultFunctionCalled = 1;
 
-  instance->gatherData = Valve_gatherDataDefault;
-  instance->setDutyCycle = Valve_setDutyCycleDefault;
+  instance->gatherData = (Valve_gatherData)Valve_gatherDataDefault;
+  instance->setDutyCycle = (Valve_setDutyCycle)Valve_setDutyCycleDefault;
+  instance->tick = (Valve_tick)Valve_tickDefault;
 }
 
 void Valve_gatherDataDefault(Valve* instance) {
   instance->errorStatus.bits.defaultFunctionCalled = 1;
 }
 
-void Valve_setDutyCycleDefault(Valve* instance, uint32_t ratio_pct) {
+void Valve_setDutyCycleDefault(Valve* instance, uint32_t dutyCycle_pct) {
   instance->errorStatus.bits.defaultFunctionCalled = 1;
 }
 

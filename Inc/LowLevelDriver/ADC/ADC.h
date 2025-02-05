91b9a768a8283e6d20c66276d9a45d9e5d3c0ddb
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-02-05 03:08:53 -0500

    Create ADC struct

--- Inc/LowLevelDriver/ADC/ADC.h
+++ Inc/LowLevelDriver/ADC/ADC.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include "../../../sirius-headers-common/LowLevelDriver/ADC/ADCErrorStatus.h"
+#include "../../../sirius-headers-common/LowLevelDriver/ADC/ADCStatus.h"
+
+struct ADC;
+
+typedef void (*ADC_init)(struct ADC* instance);
+
+typedef void (*ADC_readValue)(struct ADC* instance);
+
+typedef int16_t (*ADC_getValue)(struct ADC* instance);
+
+typedef struct {
+  int16_t currentValue;
+
+  int16_t disconnectedValue;
+  int16_t maxValue;
+  int16_t minValue;
+  int16_t shortCircuitValue;
+  
+  ADCErrorStatus  errorStatus;
+  ADCStatus       status;
+}
+ADC;

7824e9495e83e7906f9a0f56da847db676c98778
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-02-24 14:48:20 -0500

    Correct multiple def static variable

--- Src/Sensor/PressureSensor/ETM375.c
+++ Src/Sensor/PressureSensor/ETM375.c
@@ -1,13 +1,13 @@
 #include "../../../Inc/Sensor/PressureSensor/ETM375.h"
 
-PressureSensorData currentData = {0};
-
 void ETM375_init(PressureSensor* instance) {
   instance->errorStatus.value = 0;
   instance->status.value = 0;
 }
 
 void ETM375_gatherData(PressureSensor* instance) {
+  PressureSensorData currentData = {0};
+  
   // READ DIFF ADC
   return currentData;
 }
--- Src/Sensor/TemperatureSensor/NTC3950.c
+++ Src/Sensor/TemperatureSensor/NTC3950.c
@@ -1,13 +1,13 @@
 #include "../../../Inc/Sensor/TemperatureSensor/NTC3950.h"
 
-TemperatureSensorData currentData = {0};
-
 void NTC3950_init(TemperatureSensor* instance) {
   instance->status.value = 0;
   instance->errorStatus.value = 0;
 }
 
 TemperatureSensorData NTC3950_readData(TemperatureSensor* instance) {
+  TemperatureSensorData currentData = {0};
+  
   currentData.rawTemperature = instance->adc->getValue(instance->adc);
   return currentData;
 }

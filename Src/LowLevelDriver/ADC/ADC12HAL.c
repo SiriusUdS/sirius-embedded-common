77b36769a946eaf14517f6d8cf8248780c2c8bd7
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-03-01 18:18:38 -0500

    Add missing parameter

--- Src/LowLevelDriver/ADC/ADC12HAL.c
+++ Src/LowLevelDriver/ADC/ADC12HAL.c
@@ -2,10 +2,11 @@
 
 #include "stm32f4xx_hal.h"
 
-void ADC12HAL_init(ADC12* instance) {
+void ADC12HAL_init(ADC12* instance, uint8_t activeChannelsAmt) {
   instance->errorStatus.value = 0;
   instance->status.value = 0;
   ADC_HandleTypeDef* adcHandle = (ADC_HandleTypeDef*)instance->externalHandle;
+  instance->activeChannelsAmt = activeChannelsAmt;
 
   for (uint8_t i = 0; i < instance->activeChannelsAmt; i++) {
     instance->channels[i].currentValue = &instance->values[i];

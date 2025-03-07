20cf79a20dc18969935766800a57611048e3820d
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-03-07 06:10:17 -0500

    Another 2 errors

--- Inc/Sensor/TemperatureSensor/NTC3950.h
+++ Inc/Sensor/TemperatureSensor/NTC3950.h
@@ -4,4 +4,4 @@
 
 extern void NTC3950_init(TemperatureSensor* instance);
 
-extern void NTC3950_tick(TemperatureSensor* instance, uint32_t timestamp_ms);
+extern void NTC3950_tick(TemperatureSensor* instance);

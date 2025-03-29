69738bf7040fb9e1c00c10151e7f685d7d231dd7
Laurent Breton <laurent.breton03@gmail.com>
2025-03-29 14:07:49 -0400

    adds reading

--- Src/Device/Storage/SDCard.c
+++ Src/Device/Storage/SDCard.c
@@ -3,10 +3,9 @@
 #include <string.h>
 #include <stdint.h>
 
-static FRESULT SDCard_mount(Storage* instance,TCHAR *path) {
-  FATFS fatfs;
+static FRESULT SDCard_mount(Storage* instance,TCHAR *path, FATFS * fatfs) {
   FRESULT fr_status;
-  fr_status = f_mount(&fatfs, path, 1);
+  fr_status = f_mount(fatfs, path, 1);
   if (fr_status != FR_OK) {
     instance->errorStatus.bits.notInitialized = 1;
   }
@@ -23,14 +22,14 @@ static FRESULT SDCard_unmount(Storage* instance) {
   return fr_status;
 }
 
-static FRESULT SDCard_size_free_space(Storage* instance,uint32_t *total_sectors, uint32_t *free_sectors) {
-  FATFS fatfs;
+static FRESULT SDCard_size_free_space(Storage * instance, uint16_t * total_sectors, uint16_t * free_sectors, FATFS * fatfs) {
+
   FRESULT fr_status;
   DWORD free_clusters;
   fr_status = f_getfree("", free_clusters, &fatfs);
   if (fr_status == FR_OK) {
-    *total_sectors = (fatfs.n_fatent - 2) * fatfs.csize;
-    *free_sectors = free_clusters * fatfs.csize;
+    *total_sectors = (fatfs->n_fatent - 2) * fatfs->csize;
+    *free_sectors = free_clusters * fatfs->csize;
   }
   return fr_status;
 }
@@ -41,42 +40,84 @@ void SDCard_init(Storage* instance) {
 }
 
 void SDCard_store4kbData(Storage* instance, uint8_t* data) {
-  
-  if(instance->errorStatus.bits.notInitialized) {
-    if(SDCard_mount(instance, "accelerometer") != FR_OK)
-    {
-      return; // early return if mounting failed
-    }
-  }
-  
   FATFS fatfs;
-  FIL fil;
-  UINT bytes_written;
-  uint16_t free_sectors;
-  uint16_t total_sectors;
-  char buffer[100];
+
+  FRESULT fr_status = SDCard_mount(instance, "", &fatfs);
+  if (fr_status != FR_OK) {
+    instance->errorStatus.bits.notInitialized = 1;
+    return;
+  }
   uint16_t total_sectors = 0;
   uint16_t free_sectors = 0;
-
-  SDCard_size_free_space(instance, &total_sectors, &free_sectors);
-  if (free_sectors < 100) {
+  fr_status = SDCard_size_free_space(instance, &total_sectors, &free_sectors, &fatfs);
+  if (fr_status != FR_OK) {
+    instance->errorStatus.bits.notInitialized = 1;
     return;
   }
-
-  FRESULT status = f_open(&fil, "accelerometer.txt", FA_WRITE | FA_OPEN_APPEND);
-
-  if (status != FR_OK) {
+  if (free_sectors < 4) {
+    instance->errorStatus.bits.notInitialized = 1;
     return;
   }
-  
-  sprintf(buffer, "%d,%d,%d,%d,\n", data.packet.rawData->members.data.rawX, data.packet.rawData->members.data.rawY, data.packet.rawData->members.data.rawZ);
-  f_puts(buffer, &fil);
-
-  f_close(&fil);
-
-  SDCard_unmount(instance);
+  FIL file;
+  fr_status = f_open(&file, "data.txt", FA_OPEN_APPEND | FA_WRITE);
+  if (fr_status != FR_OK) {
+    instance->errorStatus.bits.notInitialized = 1;
+    return;
+  }
+  UINT bytes_written;
+  fr_status = f_write(&file, data, 4096, &bytes_written);
+  if (fr_status != FR_OK) {
+    instance->errorStatus.bits.notInitialized = 1;
+    return;
+  }
+  if (bytes_written != 4096) {
+    instance->errorStatus.bits.notInitialized = 1;
+    return;
+  }
+  fr_status = f_close(&file);
+  if (fr_status != FR_OK) {
+    instance->errorStatus.bits.notInitialized = 1;
+    return;
+  }
+  fr_status = SDCard_unmount(instance);
+  if (fr_status != FR_OK) {
+    instance->errorStatus.bits.notInitialized = 1;
+    return;
+  }
 }
 
 void SDCard_fetch4kbData(Storage* instance, uint8_t* data) {
-  
+  FATFS fatfs;
+
+  FRESULT fr_status = SDCard_mount(instance, "", &fatfs);
+  if (fr_status != FR_OK) {
+    instance->errorStatus.bits.notInitialized = 1;
+    return;
+  }
+  FIL file;
+  fr_status = f_open(&file, "data.txt", FA_READ);
+  if (fr_status != FR_OK) {
+    instance->errorStatus.bits.notInitialized = 1;
+    return;
+  }
+  UINT bytes_read;
+  fr_status = f_read(&file, data, 4096, &bytes_read);
+  if (fr_status != FR_OK) {
+    instance->errorStatus.bits.notInitialized = 1;
+    return;
+  }
+  if (bytes_read != 4096) {
+    instance->errorStatus.bits.notInitialized = 1;
+    return;
+  }
+  fr_status = f_close(&file);
+  if (fr_status != FR_OK) {
+    instance->errorStatus.bits.notInitialized = 1;
+    return;
+  }
+  fr_status = SDCard_unmount(instance);
+  if (fr_status != FR_OK) {
+    instance->errorStatus.bits.notInitialized = 1;
+    return;
+  }
 }

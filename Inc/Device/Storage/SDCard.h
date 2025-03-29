43e52df2a664386458e42a0bb591d9464a2e1cb5
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-03-29 16:19:55 -0400

    Update SDCard.h

--- Inc/Device/Storage/SDCard.h
+++ Inc/Device/Storage/SDCard.h
@@ -4,6 +4,6 @@
 
 extern void SDCard_init(Storage* instance);
 
-extern void SDCard_store4kbDataDefault(Storage* instance, uint8_t* data);
+extern void SDCard_store4kbData(Storage* instance, uint8_t* data);
 
-extern void SDCard_fetch4kbDataDefault(Storage* instance, uint8_t* data);
+extern void SDCard_fetch4kbData(Storage* instance, uint8_t* data);

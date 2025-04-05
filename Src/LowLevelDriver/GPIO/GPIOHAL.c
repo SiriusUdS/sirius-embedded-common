9a01d081ced7661bc5f2b2c765156e77778ec620
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-04-05 12:16:39 -0400

    Use HAL struct for pin group

--- Src/LowLevelDriver/GPIO/GPIOHAL.c
+++ Src/LowLevelDriver/GPIO/GPIOHAL.c
@@ -10,11 +10,11 @@ void GPIOHAL_init(GPIO* instance) {
 }
 
 GPIOValue GPIOHAL_read(GPIO* instance) {
-  instance->currentValue = (GPIOValue)HAL_GPIO_ReadPin(instance->pinGroup, instance->pinNumber);
+  instance->currentValue = (GPIOValue)HAL_GPIO_ReadPin((GPIO_TypeDef*)instance->externalHandle, instance->pinNumber);
   return instance->currentValue;
 }
 
 void GPIOHAL_write(GPIO* instance, GPIOValue value) {
   instance->currentValue = value;
-  HAL_GPIO_WritePin(instance->pinGroup, instance->pinNumber, (GPIO_PinState)instance->currentValue);
+  HAL_GPIO_WritePin((GPIO_TypeDef*)instance->externalHandle, instance->pinNumber, (GPIO_PinState)instance->currentValue);
 }

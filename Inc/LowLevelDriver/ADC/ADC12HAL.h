6481782d0623d3c352f29835d4813f7b825d00a7
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-05-15 00:51:00 -0400

    Update ADC12HAL.h

--- Inc/LowLevelDriver/ADC/ADC12HAL.h
+++ Inc/LowLevelDriver/ADC/ADC12HAL.h
@@ -2,6 +2,6 @@
 
 #include "ADC12.h"
 
-extern void ADC12HAL_init(ADC12* instance, uint8_t activeChannelsAmt);
+extern void ADC12HAL_init(ADC12* instance, ADCBuffer dmaBuffer, uint8_t activeChannelsAmt);
 
 extern void ADC12HAL_tick(ADC12* instance);

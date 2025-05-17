d4673f8cd34ee1f9969d9882eb569c573fe8580c
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-05-16 23:45:40 -0400

    put XBEE on timebase

--- Inc/Device/Telecommunication/XBEE.h
+++ Inc/Device/Telecommunication/XBEE.h
@@ -1,13 +1,15 @@
 #pragma once
 
-#include <stdint.h>
-
 #include "Telecommunication.h"
+
+#define XBEE_ENABLE_SIGNAL "b"
+#define XBEE_ENABLE_TIME_BETWEEN_SIGNALS_MS (uint8_t)50
+#define XBEE_ENABLE_SIGNAL_AMOUNT (uint8_t)0x03
 
 void XBEE_sendData(Telecommunication* instance, uint8_t* data, uint16_t size);
 
 void XBEE_receiveData(Telecommunication* instance, uint8_t* data, uint16_t size);
 
-void XBEE_setup(Telecommunication* instance);
+void XBEE_config(Telecommunication* instance);
 
 void XBEE_init(Telecommunication* instance);

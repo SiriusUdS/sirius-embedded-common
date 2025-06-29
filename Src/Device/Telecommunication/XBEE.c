2af78277422d665d7c4e771e9f1abfaeb7a76b5a
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-06-29 15:35:34 -0400

    add xbee api packet (INTEGRATION, NOT TESTED)

--- Src/Device/Telecommunication/XBEE.c
+++ Src/Device/Telecommunication/XBEE.c
@@ -4,7 +4,30 @@
 
 void XBEE_transmit(Telecommunication* instance, uint8_t* data, uint16_t size) {
   if (instance->state == TELECOMMUNICATION_STATE_ACTIVE) {
-    instance->uart->transmit((struct UART*)instance->uart, data, size);
+    XBeeSendAPIPacket api;
+    api.bits.del = DELIMITER;
+    api.bits.msb = MSB;
+    api.bits.lsb = LSB_SEND;
+    api.bits.apiFrameType = DEFAULT_API_FRAME_TYPE_SEND;
+    api.bits.frameID = 0x01;
+    
+    
+    api.bits.option = 0x00;
+    api.bits.broadcast = 0x00;
+
+    uint8_t addr[]=  {0,0,0,0,0,0,0xFF,0xFF};
+
+    for(unsigned int i=0; i < 8; i++){
+      api.bits.destAddr64[i] = addr[i];
+    }
+    api.bits.destAddr16 = 0xFFFE;
+
+    for(uint8_t i = 0; i < size; i++){
+      api.bits.data[i] = *(data+i);
+    }
+
+    api.bits.checkSum = XBEE_calculateCRCAPI(api);
+    instance->uart->transmit((struct UART*)instance->uart, api.data, SEND_PACKET_LENGHT);
   }
 }
 
@@ -37,7 +60,7 @@ void XBEE_tick(Telecommunication* instance, uint32_t timestamp_ms) {
     case TELECOMMUNICATION_STATE_ACTIVE:
       break;
     case TELECOMMUNICATION_STATE_CONFIG:
-      /*uint8_t bit[] = "b";
+      uint8_t bit[] = "b";
 
       HAL_Delay(1000);
       instance->uart->transmit((struct UART*)instance->uart, bit, sizeof(bit));
@@ -46,8 +69,11 @@ void XBEE_tick(Telecommunication* instance, uint32_t timestamp_ms) {
       HAL_Delay(2500);
       instance->uart->transmit((struct UART*)instance->uart, bit, sizeof(bit));
       HAL_Delay(500);
-      instance->state = TELECOMMUNICATION_STATE_ACTIVE;*/
-      if (instance->timestampTarget_ms <= timestamp_ms) {
+      instance->state = TELECOMMUNICATION_STATE_ACTIVE;
+      __HAL_UART_CLEAR_FLAG((UART_HandleTypeDef*)instance->uart->externalHandle, UART_FLAG_IDLE);
+      HAL_UARTEx_ReceiveToIdle_DMA((UART_HandleTypeDef*)instance->uart->externalHandle, instance->receivePacket.data, RECEIVE_PACKET_LENGHT);
+      __HAL_UART_CLEAR_FLAG((UART_HandleTypeDef*)instance->uart->externalHandle, UART_FLAG_IDLE);
+      /*if (instance->timestampTarget_ms <= timestamp_ms) {
         uint8_t bit[] = "b";
         instance->timestampTarget_ms += XBEE_ENABLE_TIME_BETWEEN_SIGNALS_MS;
         instance->uart->transmit((struct UART*)instance->uart, bit, sizeof(bit));
@@ -55,11 +81,33 @@ void XBEE_tick(Telecommunication* instance, uint32_t timestamp_ms) {
         if (instance->enableSignalsAmount >= XBEE_ENABLE_SIGNAL_AMOUNT) {
           instance->state = TELECOMMUNICATION_STATE_ACTIVE;
         }
-      }
+      }*/
       break;
     case TELECOMMUNICATION_STATE_INIT:
     case TELECOMMUNICATION_STATE_INACTIVE:
   default:
     break;
   }
+}
+
+uint8_t XBEE_calculateCRCAPI(XBeeSendAPIPacket packet)
+{
+    uint64_t calc = 0;
+
+    for(unsigned int i=0; i < LSB_SEND-14; i++){
+      calc += packet.bits.data[i];
+    }
+    calc += packet.bits.apiFrameType;
+    calc += packet.bits.broadcast;
+    calc += packet.bits.destAddr16;
+
+    uint64_t addr64 = 0;
+    for (int i = 0; i < 8; i++) {
+        addr64 |= ((uint64_t)packet.bits.destAddr64[i]) << (8 * (7 - i)); // Big-endian
+    }
+    calc += addr64;
+    calc += packet.bits.frameID;
+    calc += packet.bits.option;
+
+    return (calc & 0xFF);
 }
